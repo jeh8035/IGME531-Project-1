56f9f7fe0a358681bb94f2cf716185c36051c311
Julian <julianheuser@outlook.com>
2025-02-12 15:38:27 -0500

    Better chunking

--- CityGen/load_chunks.gd
+++ CityGen/load_chunks.gd
@@ -7,28 +7,30 @@ var chunk_asset := preload("res://CityGen/city_gen.tscn")
 
 var loaded_chunks : Dictionary
 
-func encode_coordinate(coordinate : Vector3) -> Vector2i:
+func encode_coordinate(coordinate : Vector3, offX : float = 0.5, offY : float = 0.5) -> Vector2i:
 	return Vector2i(
-		roundi(0.5 + coordinate.x / chunk_size),
-		roundi(0.5 + coordinate.z / chunk_size)
+		roundi((coordinate.x / chunk_size) + offX),
+		roundi((coordinate.z / chunk_size) + offY)
 	)
 	
 
-func decode_coordinate(key : Vector2i) -> Vector3:
+func decode_coordinate(key : Vector2i, offX : float = 0.5, offY : float = 0.5) -> Vector3:
 	return Vector3(
-		(key.x - 0.5) * chunk_size,
+		(key.x) * chunk_size,
 		0,
-		(key.y - 0.5) * chunk_size
+		(key.y) * chunk_size
 	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var trunc_pos := encode_coordinate(player_node.global_position)
-	
-	if !loaded_chunks.has(trunc_pos):
-		var asset : Node3D = chunk_asset.instantiate()
-		asset.grid_size_x = chunk_size
-		asset.grid_size_y = chunk_size
-		add_child(asset)
-		asset.global_position = decode_coordinate(trunc_pos)
-		loaded_chunks[trunc_pos] = true
+	for x in range(-1, 2):
+		for y in range(-1, 2):
+			var trunc_pos := encode_coordinate(player_node.global_position, x, y)
+			
+			if !loaded_chunks.has(trunc_pos):
+				var asset : Node3D = chunk_asset.instantiate()
+				asset.grid_size_x = chunk_size
+				asset.grid_size_y = chunk_size
+				asset.translate(decode_coordinate(trunc_pos, x, y))
+				add_child(asset)
+				loaded_chunks[trunc_pos] = true
